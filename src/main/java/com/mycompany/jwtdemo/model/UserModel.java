b3ed216f5decf282e256f907c921584cc69ba3be
IgorVHau <igorvh21@gmail.com>
2022-02-24 11:12:19 -0300

    Testing-APIs-Security-CurrentUser

--- src/main/java/com/mycompany/jwtdemo/model/UserModel.java
+++ src/main/java/com/mycompany/jwtdemo/model/UserModel.java
@@ -95,22 +95,22 @@ public class UserModel implements UserDetails {
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
